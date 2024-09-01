2aa6a8cb837bcae33d4482b9db5bd9d1e7b4bbde
mohammedbadran5 <141354875+mohammedbadran5@users.noreply.github.com>
2024-09-01 22:12:03 +0300

    Update Header.js

--- company-website-reactjs-main/company-website-reactjs-main/src/components/Header.js
+++ company-website-reactjs-main/company-website-reactjs-main/src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header>
       <video src="./video.mp4" loop autoPlay muted></video>
-      <h1>Open Source Website</h1>
+      <h1>Welcom to my first React App</h1>
       <div className="row">
         <button className="btn" style={{ cursor: "pointer" }}>
           Sign Up
